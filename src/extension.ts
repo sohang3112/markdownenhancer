a4576c4f3999c4eb5ad53a9ff2642b829ac4f9a9
Sohang Chopra <sohangchopra@gmail.com>
2024-11-08 07:11:24 +0530

    partial checkbox working in markdown preview!

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,10 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
+console.log("MarkdownEnhancer: extension.ts loaded");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('markdownenhancer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from MarkdownEnhancer!');
-	});
-
-	context.subscriptions.push(disposable);
+export function activate(ctx: vscode.ExtensionContext) {
+    // Extension activation logic, if any, can go here
+    console.log("MarkdownEnhancer extension activated!");
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
