1115f9c931b899630fb0246149768148716cba56
Sohang Chopra <sohangchopra@gmail.com>
2025-01-27 12:46:16 +0530

    work in progress

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # markdownenhancer README
-Misc Markdown enhancements.
+VS Code extension: Misc Markdown enhancements.
 
 **CHECK:** instead of new extension, is this better done using [Markdown Preview Github Styling](https://marketplace.visualstudio.com/items?itemName=bierner.markdown-preview-github-styles) extension??
 
@@ -16,6 +16,54 @@ TODO: no settings currently, maybe add any?
 ## Known Issues
 TODO: add any issue?
 
+Developer: Inspect Editor Tokens and Scopes  ---> VS Code command to debug which textmate area a piece of text comes under.
+highlight any code, run this command --> found out about markup.fenced_code.block.markdown this way only (used below)
+
+THIS WORKS (put in settings.json):
+"editor.tokenColorCustomizations": {
+    "textMateRules": [
+        {
+            "scope": [
+                // Style codeblocks without a language in markdown (this doesn't affect language-specific codeblocks, as their syntax highlighting overrides this style).
+                // in language-specific codeblocks, only the language itself is affected by this
+                "markup.fenced_code.block.markdown"       
+            ],
+            "settings": {
+                "foreground": "#6A9955"    // this is the green color used in VS Code for comments
+            }
+        }
+    ]
+}
+
 <!--
 VS Code Extension Guidelines: https://code.visualstudio.com/api/references/extension-guidelines
 -->
+
+```
+HELLO
+WORLD
+```
+
+language	markdown
+standard token type	Other
+foreground	#CCCCCC
+background	#1F1F1F
+contrast ratio	10.26
+textmate scopes	punctuation.definition.markdown
+markup.fenced_code.block.markdown
+text.html.markdown
+foreground	No theme selector
+
+```javascript
+console.log("Hello World!");
+```
+
+language	markdown
+standard token type	Other
+foreground	#CCCCCC
+background	#1F1F1F
+contrast ratio	10.26
+textmate scopes	punctuation.definition.markdown
+markup.fenced_code.block.markdown
+text.html.markdown
+foreground	No theme selector
--- media/preview.js
+++ media/preview.js
@@ -5,25 +5,24 @@ function randomNDigits(digits) {
     return Math.floor((1 + Math.random()) * base);
 }
 
-// TODO: run every time, not just once when first loaded, but also when eg. original markdown changes so dom content again changes
-// document.addEventListener('DOMContentLoaded', () => {
 window.addEventListener('load', () => {
     // Find all checkboxes with [-] syntax and set them to indeterminate
     console.log('MarkdownEnhancer: preview.js: window loaded');
-    for (let e of document.querySelectorAll('li:not(.task-list-item)')) {
-        console.log(e);
+    for (let elem of document.querySelectorAll('li:not(.task-list-item)')) {
+        console.log(elem);
         // TODO: didn't trim before checking startswith - spaces should already be handled by markdown preview (check this)
         // NOTE: textContent is more performant than innerText (because it doesn't check for eg. hidden elements), that's why it's used
         // Source: https://stackoverflow.com/a/35213639/12947681
-        if (e.textContent.startsWith('[-]')) {
+        if (elem.textContent.startsWith('[-]')) {
             const cid = `task-item-${randomNDigits(7)}`;        // checkbox id
-            const elemHtml = e.innerHTML.slice(3);              // remove "[-]" from start
-            e.innerHTML = `<input class="task-list-item-checkbox" type="checkbox" id="${cid}"> <label class="task-list-item-label" for="${cid}">${elemHtml}</label>`;
+            const elemHtml = elem.innerHTML.slice(3);              // remove "[-]" from start
+            elem.innerHTML = `<input class="task-list-item-checkbox indeterminate" type="checkbox" id="${cid}"> 
+                              <label class="task-list-item-label" for="${cid}">${elemHtml}</label>`;
             let checkbox = document.getElementById(cid);
             checkbox.indeterminate = true;
-            
-            e.classList.add('task-list-item', 'enabled');
-            e.parentElement.classList.add('contains-task-list');
+
+            elem.classList.add('task-list-item', 'enabled');       // remove list bullet that's present for non-checkbox list items
+            elem.parentElement.classList.add('contains-task-list');
         }
     }
 });
